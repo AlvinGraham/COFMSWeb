0cac2d85daeee802653a2d43870065c98c9ba3ff
Alvin Graham <grah0071@gmail.com>
2024-03-14 16:13:23 -0500

    Populate initial database data

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-
 -- Create Tables
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial NOT NULL UNIQUE,
@@ -13,14 +12,14 @@ CREATE TABLE IF NOT EXISTS "units" (
 	"type" varchar(80) NOT NULL DEFAULT '',
 	"fe" float8 NOT NULL DEFAULT 0,
 	"affiliation" varchar(16) NOT NULL DEFAULT '',
-	"country_code" varchar(8) NOT NULL DEFAULT '"US"',
+	"country_code" varchar(8) NOT NULL DEFAULT 'us',
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "countries" (
 	"id" serial NOT NULL UNIQUE,
 	"country_code" varchar(8) NOT NULL UNIQUE,
-	"name" varchar(40) NOT NULL DEFAULT '',
+	"name" varchar(50) NOT NULL DEFAULT '',
 	PRIMARY KEY ("id")
 );
 
@@ -28,8 +27,8 @@ CREATE TABLE IF NOT EXISTS "equation_coefficients" (
 	"id" serial NOT NULL UNIQUE,
 	"blue_mission" varchar(40) NOT NULL DEFAULT '',
 	"red_mission" varchar(40) NOT NULL DEFAULT '',
-	"base" float8 NOT NULL DEFAULT 0,
-	"exponent" float8 NOT NULL DEFAULT 0,
+	"base" float8 DEFAULT 0,
+	"exponent" float8 DEFAULT 0,
 	PRIMARY KEY ("id")
 );
 
@@ -53,8 +52,8 @@ CREATE TABLE IF NOT EXISTS "blue_forces" (
 
 CREATE TABLE IF NOT EXISTS "missions" (
 	"id" serial NOT NULL UNIQUE,
-	"blue_mission" varchar(40) NOT NULL DEFAULT 'meeting engagement',
-	"red_mission" varchar(40) NOT NULL DEFAULT 'meeting engagement',
+	"blue_mission" varchar(40) NOT NULL DEFAULT 'Meeting Engagement',
+	"red_mission" varchar(40) NOT NULL DEFAULT 'Meeting Engagement',
 	"user_id" int NOT NULL DEFAULT '0',
 	PRIMARY KEY ("id")
 );
@@ -65,19 +64,19 @@ CREATE TABLE IF NOT EXISTS "mission_types" (
 	PRIMARY KEY ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "missions_equtions" (
+CREATE TABLE IF NOT EXISTS "missions_equations" (
 	"id" serial NOT NULL UNIQUE,
 	"mission_id" int NOT NULL DEFAULT '0',
 	"equation_id" int NOT NULL DEFAULT '0',
 	PRIMARY KEY ("id")
 );
 
--- Establish Join Relationships
+-- Establish Relationships
 ALTER TABLE "units" ADD CONSTRAINT "units_fk4" FOREIGN KEY ("country_code") REFERENCES "countries"("country_code");
 ALTER TABLE "red_forces" ADD CONSTRAINT "red_forces_fk1" FOREIGN KEY ("unit_id") REFERENCES "units"("id");
 ALTER TABLE "red_forces" ADD CONSTRAINT "red_forces_fk4" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 ALTER TABLE "blue_forces" ADD CONSTRAINT "blue_forces_fk1" FOREIGN KEY ("unit_id") REFERENCES "units"("id");
 ALTER TABLE "blue_forces" ADD CONSTRAINT "blue_forces_fk4" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 ALTER TABLE "missions" ADD CONSTRAINT "missions_fk3" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-ALTER TABLE "missions_equtions" ADD CONSTRAINT "missions_equtions_fk1" FOREIGN KEY ("mission_id") REFERENCES "missions"("id");
-ALTER TABLE "missions_equtions" ADD CONSTRAINT "missions_equtions_fk2" FOREIGN KEY ("equation_id") REFERENCES "equation_coefficients"("id");
+ALTER TABLE "missions_equations" ADD CONSTRAINT "missions_equations_fk1" FOREIGN KEY ("mission_id") REFERENCES "missions"("id");
+ALTER TABLE "missions_equations" ADD CONSTRAINT "missions_equations_fk2" FOREIGN KEY ("equation_id") REFERENCES "equation_coefficients"("id");
