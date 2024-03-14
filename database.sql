58cf5dced0f87a3f0ab80a29f5da645e6703ed40
Alvin Graham <grah0071@gmail.com>
2024-03-13 21:04:47 -0500

    Build database structure

--- database.sql
+++ database.sql
@@ -1,9 +1,83 @@
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+
+-- Create Tables
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(80) NOT NULL DEFAULT '',
+	"password" varchar(40) NOT NULL DEFAULT '',
+	"admin" boolean NOT NULL DEFAULT false,
+	PRIMARY KEY ("id")
 );
+
+CREATE TABLE IF NOT EXISTS "units" (
+	"id" serial NOT NULL UNIQUE,
+	"type" varchar(80) NOT NULL DEFAULT '',
+	"fe" float8 NOT NULL DEFAULT 0,
+	"affiliation" varchar(16) NOT NULL DEFAULT '',
+	"country_code" varchar(8) NOT NULL DEFAULT '"US"',
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "countries" (
+	"id" serial NOT NULL UNIQUE,
+	"country_code" varchar(8) NOT NULL UNIQUE,
+	"name" varchar(40) NOT NULL DEFAULT '',
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "equation_coefficients" (
+	"id" serial NOT NULL UNIQUE,
+	"blue_mission" varchar(40) NOT NULL DEFAULT '',
+	"red_mission" varchar(40) NOT NULL DEFAULT '',
+	"base" float8 NOT NULL DEFAULT 0,
+	"exponent" float8 NOT NULL DEFAULT 0,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "red_forces" (
+	"id" serial NOT NULL UNIQUE,
+	"unit_id" int NOT NULL DEFAULT 0,
+	"quantity" int NOT NULL DEFAULT '1',
+	"strength" int NOT NULL DEFAULT '100',
+	"user_id" int NOT NULL DEFAULT 0,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "blue_forces" (
+	"id" serial NOT NULL UNIQUE,
+	"unit_id" int NOT NULL DEFAULT '0',
+	"quantity" int NOT NULL DEFAULT '1',
+	"strength" int NOT NULL DEFAULT '100',
+	"user_id" int NOT NULL DEFAULT '0',
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "missions" (
+	"id" serial NOT NULL UNIQUE,
+	"blue_mission" varchar(40) NOT NULL DEFAULT 'meeting engagement',
+	"red_mission" varchar(40) NOT NULL DEFAULT 'meeting engagement',
+	"user_id" int NOT NULL DEFAULT '0',
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "mission_types" (
+	"id" serial NOT NULL UNIQUE,
+	"mission" varchar(40) NOT NULL DEFAULT '',
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "missions_equtions" (
+	"id" serial NOT NULL UNIQUE,
+	"mission_id" int NOT NULL DEFAULT '0',
+	"equation_id" int NOT NULL DEFAULT '0',
+	PRIMARY KEY ("id")
+);
+
+-- Establish Join Relationships
+ALTER TABLE "units" ADD CONSTRAINT "units_fk4" FOREIGN KEY ("country_code") REFERENCES "countries"("country_code");
+ALTER TABLE "red_forces" ADD CONSTRAINT "red_forces_fk1" FOREIGN KEY ("unit_id") REFERENCES "units"("id");
+ALTER TABLE "red_forces" ADD CONSTRAINT "red_forces_fk4" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "blue_forces" ADD CONSTRAINT "blue_forces_fk1" FOREIGN KEY ("unit_id") REFERENCES "units"("id");
+ALTER TABLE "blue_forces" ADD CONSTRAINT "blue_forces_fk4" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "missions" ADD CONSTRAINT "missions_fk3" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "missions_equtions" ADD CONSTRAINT "missions_equtions_fk1" FOREIGN KEY ("mission_id") REFERENCES "missions"("id");
+ALTER TABLE "missions_equtions" ADD CONSTRAINT "missions_equtions_fk2" FOREIGN KEY ("equation_id") REFERENCES "equation_coefficients"("id");
