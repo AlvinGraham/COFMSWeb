b107818b982313d6f6ad6563d641816e9a071032
Alvin Graham <grah0071@gmail.com>
2024-03-20 20:46:34 -0500

    Create server forces router

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ const passport = require('./strategies/user.strategy');
 
 // Route Includes
 const userRouter = require('./routes/user.router');
+const forcesRouter = require('./routes/forces.router');
 
 // Express Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 // Passport Session Configuration
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/forces', userRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
