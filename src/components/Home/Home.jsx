d2692329c31b552cfc715910d386260315550c60
Alvin Graham <grah0071@gmail.com>
2024-04-09 20:46:39 -0500

    Update Home component formatting and layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,30 +12,23 @@ function Home(props) {
   }, []);
   return (
     <div id="home-div">
-      <div className="left test-box">
+      <div className="left">
+        <h1>COFMSWeb 1.0</h1>
+        <p>Welcome to COFMSWeb!</p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at ex nec
-          nulla finibus lobortis. Cras in risus leo. Quisque dapibus facilisis
-          magna, et pharetra nunc elementum pellentesque. Mauris at lectus
-          lobortis ligula ullamcorper luctus sit amet quis eros. Fusce porttitor
-          nec lacus sed euismod. Duis tincidunt lectus vitae sagittis tristique.
-          Maecenas ultricies vitae neque sit amet rutrum. Proin nec elit
-          pretium, fringilla dui sed, fringilla enim. Aenean nec varius sapien.
-          Vivamus molestie eu neque sed ultricies. Nunc imperdiet et turpis
-          vitae venenatis. Sed vitae lacus vulputate, scelerisque justo a,
-          imperdiet ex. Mauris tortor arcu, mollis id libero id, vehicula
-          dignissim lacus. Phasellus fringilla urna ac maximus lacinia. Maecenas
-          faucibus velit iaculis, fermentum elit quis, laoreet urna. Mauris
-          vitae facilisis risus, a imperdiet mauris. Nullam finibus et ligula
-          eget pellentesque. Nullam laoreet et quam accumsan lobortis. Praesent
-          quis est id nulla sagittis molestie imperdiet ac dui. Donec congue
-          lectus at iaculis tempus. Nam consequat sollicitudin laoreet. Cras
-          gravida ex sed ex tincidunt, in pellentesque dui ullamcorper. Nam id
-          sodales ipsum. Quisque in felis id nibh blandit convallis. Vestibulum
-          eu sodales eros.
+          &nbsp; COFMSWeb (Correlation Of Forces and MeanS - Web Implementation)
+          is a fullstack application based on a US Army force ratio calculator.
+          It allows staff planners to analyze relative force ratios and
+          determine likelihood of mission success based on correlations of
+          conventional combat power of opposing forces. Users have access to a
+          pre-defined array of conventional forces and administrative users have
+          the ability to add, modify, or delete existing forces. Comparisons are
+          displayed graphically and can be exported to PDF for inclusion in
+          other applications or briefing products.
         </p>
+        <img src="src/components/Home/images/staff.jpg" />
       </div>
-      <div className="right test-box">
+      <div className="right">
         <img
           src="src/components/Home/images/tank.jpeg"
           alt="Army Tank"
